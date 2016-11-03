f53c5919550211b9d627858893029cd972e5d853
root <root@cci-odp-data.cems.rl.ac.uk>
2016-11-03 20:59:59 +0000

    default to not adding per-file WMS links on data node

--- add_wms_for_data_node.py
+++ add_wms_for_data_node.py
@@ -21,16 +21,21 @@ class ThreddsXMLDatasetOnDataNode(ThreddsXMLDatasetBase):
     def __init__(self, 
                  wms_url_base = 'https://cci-odp-data.cems.rl.ac.uk/thredds/wms/',
                  wcs_url_base = 'https://cci-odp-data.cems.rl.ac.uk/thredds/wcs/',
+                 do_per_file_links = False,
                  do_wcs = False,
                  **kwargs):
         ThreddsXMLDatasetBase.__init__(self, **kwargs)
         self.wms_url_base = wms_url_base
         self.wcs_url_base = wcs_url_base
         self.do_wcs = do_wcs
+        self.do_per_file_links = do_per_file_links
 
     def insert_access_links(self):        
         dsid = self.dataset_id
-        for ds in [self.top_level_dataset] + self.second_level_datasets:
+        dsets = [self.top_level_dataset]
+        if self.do_per_file_links:
+            dsets += self.second_level_datasets
+        for ds in dsets:
             access = self.new_element("access", serviceName="wms", urlPath=dsid)
             self.insert_element_before_similar(ds, access)
             if self.do_wcs:
