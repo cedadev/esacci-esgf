b64aada3e0175336367b1684d4fa8140b908d848
Joe Singleton <joesingo@gmail.com>
2018-01-10 11:22:57 +0000

    Add OpenDAP access for aggregate datasets
    Add a new <service> element linking to OpenDAP on the WMS server
    (where aggregations are stored) and add this service as an access
    method for the top-level dataset

--- add_wms_for_data_node.py
+++ add_wms_for_data_node.py
@@ -10,6 +10,7 @@ For default filenames used, see default args to ProcessBatch.__init__()
 
 import sys
 import os
+import copy
 
 from addwms_base import ThreddsXMLDatasetBase, ProcessBatchBase
 
@@ -18,15 +19,22 @@ class ThreddsXMLDatasetOnDataNode(ThreddsXMLDatasetBase):
     A class for processing THREDDS XML files and tweaking them to add WMS tags
     that point to external WMS server
     """
+
+    # The name to use in the 'service' element for linking to OpenDAP on the
+    # WMS server - needs to not clash with local OpenDAP service.
+    REMOTE_OPENDAP_SERVICE_NAME = "OpenDAP-remote"
+
     def __init__(self, 
                  wms_url_base = 'https://cci-odp-data.cems.rl.ac.uk/thredds/wms/',
                  wcs_url_base = 'https://cci-odp-data.cems.rl.ac.uk/thredds/wcs/',
+                 remote_opendap_base = 'https://cci-odp-data.cems.rl.ac.uk/thredds/dodsC/',
                  do_per_file_links = False,
                  do_wcs = False,
                  **kwargs):
         ThreddsXMLDatasetBase.__init__(self, **kwargs)
         self.wms_url_base = wms_url_base
         self.wcs_url_base = wcs_url_base
+        self.remote_opendap_base = remote_opendap_base
         self.do_wcs = do_wcs
         self.do_per_file_links = do_per_file_links
 
@@ -36,15 +44,29 @@ class ThreddsXMLDatasetOnDataNode(ThreddsXMLDatasetBase):
         if self.do_per_file_links:
             dsets += self.second_level_datasets
         for ds in dsets:
-            access = self.new_element("access", serviceName="wms", urlPath=dsid)
-            self.insert_element_before_similar(ds, access)
+            services = ["wms", self.REMOTE_OPENDAP_SERVICE_NAME]
             if self.do_wcs:
-                access = self.new_element("access", serviceName="wcs", urlPath=dsid)
+                services.append("wcs")
+            for service_name in services:
+                access = self.new_element("access", serviceName=service_name, urlPath=dsid)
                 self.insert_element_before_similar(ds, access)
 
     def all_changes(self):
         self.insert_viewer_metadata()
         self.insert_wms_service(base = self.wms_url_base)
+
+        # Copy OpenDAP service and change base to WMS server
+        services = self.root.findall(self.tag_full_name("service"))
+        for sv in services:
+            if sv.get("serviceType") == "OpenDAP":
+                new_sv = copy.deepcopy(sv)
+                new_sv.set("name", self.REMOTE_OPENDAP_SERVICE_NAME)
+                new_sv.set("base", self.remote_opendap_base)
+                self.root.insert(0, new_sv)
+                break
+        else:
+            raise ValueError("Could not find OpenDAP service")
+
         if self.do_wcs:
             self.insert_wcs_service(base = self.wcs_url_base)
         self.insert_access_links()
