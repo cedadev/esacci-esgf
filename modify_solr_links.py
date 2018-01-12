7b136b19b565f2d6da745233b11f40c6300918d9
Joe Singleton <joesingo@gmail.com>
2018-01-12 17:05:21 +0000

    Update modify_solr_links.py to remove '.html' suffix from OpenDAP endpoints in index
    The OpenDAP URL with '.html' is the URL to the web UI, but is it more useful to
    have a URL that can be used directly with an OpenDAP client.

--- modify_solr_links.py
+++ modify_solr_links.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+import sys
+import os
 
 import pysolr
 
 import string
 
 """
-Script that modifies the WMS links in Solr.
+Script that modifies the WMS, WCS and OpenDAP links in Solr.
 """
+
+DEFAULT_SOLR_NODE = 'http://esgf-index1.ceda.ac.uk:8984'
 
 def solr_update_doc(s, doc, func):
     """
@@ -43,11 +47,12 @@ def query_all(s, query="*.*", chunk=1000):
         results.extend(these_results)
         start += chunk
 
-
-def update_wms_and_wcs_urls(doc):
+def update_urls(doc):
     """
     Update the WMS and/or WCS URLs in the "url" list in the document so as to add the
     query parameters needed for GetCapabilities if they are not already there.
+    Update any OpenDAP URLs to remove the .html suffix.
+
     Works equally with a dataset document and a file document.
 
     Returns boolean to say if updated
@@ -58,17 +63,40 @@ def update_wms_and_wcs_urls(doc):
         bits = url_with_svc.split('|')
         if bits[2] == 'WMS' and '?' not in bits[0]:
             bits[0] += '?service=WMS&version=1.3.0&request=GetCapabilities'
-            urls[i] = string.join(bits, '|')
             changed = True
         # WCS similar but note different version
         if bits[2] == 'WCS' and '?' not in bits[0]:
             bits[0] += '?service=WCS&version=1.0.0&request=GetCapabilities'
-            urls[i] = string.join(bits, '|')
             changed = True
+        # If OpenDAP then remove .html suffix
+        if bits[2] == 'OPENDAP' and bits[0].endswith('.html'):
+            bits[0] = bits[0][:-5]
+            changed = True
+
+        urls[i] = string.join(bits, '|')
+
     return changed
 
+def usage():
+    prog = os.path.basename(sys.argv[0])
+    print("""Usage: %s [SOLR NODE]
+
+Modify Solr documents to correct WMS, WCS and OpenDAP endpoints for
+aggregate datasets
+
+SOLR NODE defaults to %s""" % (prog, DEFAULT_SOLR_NODE))
+
 if __name__ == '__main__':
-    s = pysolr.Solr('http://esgf-index1.ceda.ac.uk:8984/solr/datasets')
+
+    solr_node = DEFAULT_SOLR_NODE
+    if len(sys.argv) > 1:
+        if sys.argv[1] in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        else:
+            solr_node = sys.argv[1]
+
+    s = pysolr.Solr("%s/solr/datasets" % solr_node)
     dsets = query_all(s, query='esacci')
     for ds in dsets:
-        solr_update_doc(s, ds, update_wms_and_wcs_urls)
+        solr_update_doc(s, ds, update_urls)
